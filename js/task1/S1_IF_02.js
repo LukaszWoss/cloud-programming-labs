0305c8cd67f3a7f4916556ad88d0173246b68e93
LukaszWoss <lukaszwo0ss@gmail.com>
2026-01-30 03:50:21 +0100

    task 2

---
+++ js/task1/S1_IF_02.js
@@ -0,0 +1,17 @@
+// S1_IF_02
+
+function grade(score) {
+    if (score < 0 || score > 100) {
+        return null;
+    }
+    if (score >= 90) return "A";
+    if (score >= 80) return "B";
+    if (score >= 70) return "C";
+    if (score >= 60) return "D";
+    
+    return "F";
+}
+
+console.log(grade(95));
+console.log(grade(55));
+console.log(grade(105)); // null

ec0e846a6d8d780d6f6e7d267cf2af2b0c891e9b
LukaszWoss <lukaszwo0ss@gmail.com>
2026-01-30 03:09:03 +0100

    TASK1: S1_VAR_01 - variables table

--- js/task1/S1_VAR_01.js
+++ js/task1/S1_VAR_01.js
@@ -5,7 +5,7 @@ let myNumber = 123;         // number
 const myBool = true;        // boolean
 
 console.table([
-    { variable: "myText",   value: myText,   type: typeof myText },
+    { variable: "mytext",   value: myText,   type: typeof myText },
     { variable: "myNumber", value: myNumber, type: typeof myNumber },
     { variable: "myBool",   value: myBool,   type: typeof myBool }
 ]);
